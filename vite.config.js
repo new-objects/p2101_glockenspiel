56d7cbfb6b0354e7599d9ab735406c7b8291b1c4
mi-skam <40042054+mi-skam@users.noreply.github.com>
2024-02-18 08:56:40 +0100

    fix in DEPLOY_ENV

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-  base:
-    process.env.DEPLOY_ENV === 'gh-pages' ? '/glockenspiel/' : '/',
+  base: process.env.DEPLOY_ENV === 'gh-pages' ? '/p2101_glockenspiel/' : '/',
   plugins: [
     eslintPlugin({
       cache: false,
